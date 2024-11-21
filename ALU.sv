8469cd510bfdbe0312084a2210379b7f08199dc6
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-11-21 10:17:42 +0300

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 
 
-module ALU(
-    input logic [7:0] a,          
-    input logic [7:0] b,          
+module ALU# (parameter N = 8)(
+    input logic [N-1:0] a,          
+    input logic [N-1:0] b,          
     input logic op,               
-    output logic [7:0] result,    
+    output logic [N-1:0] result,    
     output logic carry          
 );
     
-    logic [7:0]bcomp;
-    logic [7:0]b2sout;
+    logic [N-1:0]bcomp;
+    logic [N-1:0]b2sout;
     
     assign bcomp= ~b+1;
            
-    MUX #(.N(8)) b2comp(
+    MUX #(.N(N)) b2comp(
     .a(b),
     .b(bcomp),
     .s(op),
