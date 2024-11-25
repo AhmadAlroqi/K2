1050b5b88b8e3e1e26f60746da9237b113d3f2e0
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-11-25 16:21:47 +0300

    Update sev_seg.sv

--- sev_seg.sv
+++ sev_seg.sv
@@ -1,6 +1,6 @@
 module sev_seg_top(
     input wire CLK100MHZ,    // using the same name as pin names
-    input wire CPU_RESETN,   
+    input wire CPU_RESETN,BTNC,
     output wire CA, CB, CC, CD, CE, CF, CG, DP,
     output wire [7:0] AN,    
     input wire [15:0] SW     
@@ -17,15 +17,29 @@ assign clk = CLK100MHZ;
 // Seven segments Controller
 wire [6:0] Seg;
 wire [3:0] digits[0:7];
+wire [7:0] out_SW;
+wire [7:0] out_SW1;
+wire [7:0] out_SW2;
 
-assign digits[0] = SW[3:0];
-assign digits[1] = SW[7:4];
-assign digits[2] = SW[11:8];
-assign digits[3] = SW[15:12];
+
+            K2TOP(
+        .clk(BTNC),
+        .reset_n(reset_n),
+        .R0(out_SW)
+        ,.RA(out_SW1),
+        .RB(out_SW2)
+    );
+    
+    
+assign digits[0] = out_SW1[3:0];
+assign digits[1] = out_SW1[7:4];
+assign digits[2] = out_SW2[3:0];
+assign digits[3] = out_SW2[7:4];
 assign digits[4] = 4'b1111;
 assign digits[5] = 4'b1111;
-assign digits[6] = 4'b1111;
-assign digits[7] = 4'b1111;
+assign digits[6] = out_SW[3:0];
+assign digits[7] = out_SW[7:4]
+;
 
 
 sev_seg_controller ssc(
