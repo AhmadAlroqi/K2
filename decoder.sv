fdc243ce99cf871e70a0c4f18882d3bc4aa92a43
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-11-25 16:20:45 +0300

    Add files via upload

--- decoder.sv
+++ decoder.sv
@@ -1,16 +1,10 @@
-`timescale 1ns / 1ps
-
-module decoder(
-    input logic [1:0] in,   
-    output logic [3:0] out   
+module decoder#(
+    parameter n = 2
+)(
+    input logic [n-1:0]in,
+    output logic [(1 << n) - 1: 0]out
 );
-    always @(*) begin
-            case (in)
-                2'b00: out = 1; //RA
-                2'b01: out = 2; //RB
-                2'b10: out = 4; //R0
-                default: out = 4'b0000;
-            endcase
-        
-     end
-endmodule
+
+    assign out = (1 << in);
+
+endmodule : decoder
