bec562229f10634d25cdc313da5fdc1adc99bb56
AmrBinMuhsinah <amr.bin.mohsenh@gmail.com>
2024-11-25 13:18:12 +0300

    Add files via upload

--- RAM.sv
+++ RAM.sv
@@ -1,33 +1,22 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/21/2024 03:14:39 PM
-// Design Name: 
-// Module Name: RAM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 module RAM(
-input logic clk,
-input logic en,
-input logic [7:0]address,
-input logic [7:0]data_in,
-output logic [7:0]data_out
- );
- 
- 
- alwyas
+  
+    input logic clk,
+    input logic  write_enable,
+    input logic [9:0]address,
+    input logic [7:0]data_in,
+    output logic  [7:0]data_out
+    );
+   
+
+logic [7:0]ram_block[15:0];
+
+always @(posedge clk) begin
+        if(write_enable)
+            ram_block[address] <= data_in;
+end
+  assign data_out = ram_block[address];
 endmodule
